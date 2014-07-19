ee4a469acc2527ef59d2f4c305d72fe4ef50da1d
Boris Gribovskiy <xinu@yandex.ru>
2014-07-19 18:28:33 +0400

    v1.1.4  Timing (workHours, fanHours, pumpHours) LCD output format

--- parnik.ino
+++ parnik.ino
@@ -2,7 +2,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-const char version[] = "1.1.3";
+const char version[] = "1.1.4";
 
 const int knob1Pin = A2;
 const int knob2Pin = A3;
@@ -42,29 +42,42 @@ int fanState = 0;
 int pumpState = 0;
 int i = 0; // iteration counter;
 float navg ; // number of samples for averaging
+float workHours, fanHours, pumpHours; // work time (hours)
+unsigned long fanMillis, pumpMillis, workMillis, delta;
 
 void setup(void) {
   Serial.begin(9600);
   sensors.begin();
   lcd.begin(20, 4);
   lcd.clear();
-  lcd.setCursor(14,0);
+  lcd.setCursor(0,0);
+  lcd.print("v=");
+  lcd.setCursor(2,0);
   lcd.print(version);
-  lcd.setCursor(0, 0);
-  lcd.print("Humid=");
+  
   lcd.setCursor(0, 1);
-  lcd.print("Porog=");
+  lcd.print("U=");
+  
   lcd.setCursor(0, 2);
-  lcd.print("Temp=");
+  lcd.print("T=");
+  lcd.setCursor(7, 2);
+  lcd.print("/");
+  
   lcd.setCursor(0, 3);
-  lcd.print("Humidity=");
+  lcd.print("H=");
+  lcd.setCursor(7, 3);
+  lcd.print("/");
+  
   pinMode(fanPin, OUTPUT);
   pinMode(pumpPin, OUTPUT);
   digitalWrite(fanPin, LOW);
   digitalWrite(pumpPin, LOW);
   fanState = 0; 
-  pumpState = 0; 
+  fanMillis = 0;
+  pumpState = 0;
+  pumpMillis = 0; 
   Serial.println(version);
+  workMillis = millis();
 }
 
 void loop(void) {
@@ -78,6 +91,18 @@ void loop(void) {
   }   
   q = 1.0/navg;
   p = 1.0 - q;
+  // Timing
+  delta = millis() - workMillis;
+  workMillis += delta;
+  if (fanState) {
+    fanMillis += delta;
+  }
+  if (pumpState) {
+    pumpMillis += delta;
+  }
+  workHours = workMillis/3600000.; // Hours;
+  fanHours = fanMillis/3600000.;
+  pumpHours = pumpMillis/3600000.;
   
   // read the value from the input
   knob1Value = (float)analogRead(knob1Pin);
@@ -91,7 +116,7 @@ void loop(void) {
   temp_lo = gain*knob1Value;
   temp_hi = temp_lo + 1.0;
   hum_lo = knob2Value;
-  hum_hi = hum_lo + 100.;
+  hum_hi = hum_lo + 2.;
   sensors.requestTemperatures();
   temp = sensors.getTempCByIndex(0);
   humidity = 1.0 * humValue;
@@ -99,16 +124,25 @@ void loop(void) {
   volt = 12.77/2.55*3./1023.* dividerValue;
   volt_avg = p*volt_avg + q*volt;
   
-  lcd.setCursor(6, 0);
-  lcd.print(hum_lo);
-  lcd.setCursor(6, 1);
-  lcd.print(temp_lo); 
-  lcd.setCursor(5, 2);
+  lcd.setCursor(14, 0);
+  lcd.print(workHours);
+  
+  lcd.setCursor(2, 1);
+  lcd.print(volt_avg); 
+  
+  lcd.setCursor(2, 2);
   lcd.print(temp);  
+  lcd.setCursor(8, 2);
+  lcd.print(temp_lo);
   lcd.setCursor(14, 2);
-  lcd.print(volt_avg);
-  lcd.setCursor(9, 3);
+  lcd.print(fanHours);
+  
+  lcd.setCursor(2, 3);
   lcd.print(humidity_avg);
+  lcd.setCursor(8, 3);
+  lcd.print(hum_lo);
+  lcd.setCursor(14, 3);
+  lcd.print(pumpHours);
   
  /* fan control */ 
   if (fanState == 1) {
@@ -134,30 +168,8 @@ void loop(void) {
        pumpState = 1;
      } 
   }  
+}  
   
-  //pulse();
-  /*
-  digitalWrite(ledPin, HIGH);   // set the LED on
-  delay(200);              // wait for a second
-  digitalWrite(ledPin, LOW);    // set the LED off
-  delay(800);              // wait for a second
-  */
-  
-  //Serial.print("Requesting temperatures...");
-  //sensors.requestTemperatures(); // Send the command to get temperatures
-  //Serial.println("DONE");
-  //temp = sensors.getTempCByIndex(0);
-  //Serial.print("Temperature for the device 1 (index 0) is: ");
-  //Serial.println(temp);  
-}
-/*
-void pulse() {
-  count++;
-  next = millis();
-  delta = (next - last);
-  last = next;
-}
-*/  
 
 
 
